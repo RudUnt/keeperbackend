cc20a5174d30d6f0a7b9ccc835732cd9a76f8fdc
RudUnt <rudreshuntwal26@gmail.com>
2024-05-30 21:40:05 +0530

    location change of index.js related to env.Node

--- index.js
+++ index.js
@@ -62,13 +62,13 @@ app.use(passport.session());
 //   content: "Buy Milk, Buy Cookies, Buy Vegetables"
 // }]
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+//   });
+// }
 
 
 app.use('/api', authRoutes);
